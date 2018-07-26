46d063f4b0e07998e432d5713214c3e42def3f6d
RobertoPanoi <roberto@panoimagen.com>
2018-07-26 11:36:48 +0200

    Updated version number to 2.2.67

--- version.py
+++ version.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 # This file has been generated automatically
 # using version.py, do not update manually!!
 
-VERSION = """2.2.66"""
+VERSION = """2.2.67"""
 
 if __name__ == "__main__":
     print(VERSION)
