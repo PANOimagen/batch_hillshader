d74503ac37c5fb0c7748adff5f836d4172d09694
RobertoPanoi <roberto@panoimagen.com>
2018-09-28 09:49:05 +0200

    Updated version number and metadata.txt

--- version.py
+++ version.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 # This file has been generated automatically
 # using version.py, do not update manually!!
 
-VERSION = """2.2.69"""
+VERSION = """2.3.0"""
 
 if __name__ == "__main__":
     print(VERSION)
