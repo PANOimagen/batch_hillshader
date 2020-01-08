8c042327676a725b5363549cee04618e78451b06
RobertoCSH <31844800+RobertoPanoi@users.noreply.github.com>
2020-01-08 10:32:06 +0100

    Updated version

--- version.py
+++ version.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 # This file has been generated automatically
 # using version.py, do not update manually!!
 
-VERSION = """2.4.0"""
+VERSION = """2.4.1"""
 
 if __name__ == "__main__":
     print(VERSION)
